d182793a50f710cfbdb739e85c426ec80b3ec8c6
Harikumar-pixel <114511122+Harikumar-pixel@users.noreply.github.com>
2022-10-06 23:19:09 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,39 @@
+// varibles & datatypes
+
+//string
+var person= "john";
+let person1 ="smith";
+document.write(typeof person1)
+document.write("<br>") //for next line
+document.write(person);
+
+document.write("<br>") //for next line
+//number
+var number = 10;
+var number1 = 20.33;
+document.write(typeof number)
+document.write("<br>") //for next line
+document.write(number1);
+
+document.write("<br>") //for next line
+//boolean
+var js=true;
+var html=false;
+document.write(typeof html)
+document.write("<br>") //for next line
+document.write(html);
+
+document.write("<br>") //for next line
+//undefined
+var test1;
+document.write(typeof test1)
+document.write("<br>") //for next line
+document.write(test1);
+
+//null
+var test2=null;
+
+document.write("<br>") //for next line
+//constant
+const pi=3.14;
+document.write(pi);
